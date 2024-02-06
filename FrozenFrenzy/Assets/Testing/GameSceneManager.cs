1da7a21f5601dc27cd7e9602083092e7ca59f099
kiana <kianaleslie25@gmail.com>
2024-02-06 14:08:44 -0700

    just moved some things, working on the menu right now

---
+++ FrozenFrenzy/Assets/Testing/GameSceneManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameSceneManager : MonoBehaviour
+{
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void LoadGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
