11a4075b8d0471087992d2e3a56cb5e675e88c8f
Maxwell Boucher <112987961+mboucher41@users.noreply.github.com>
2024-02-06 14:43:44 -0700

    set up new controls, touch possibly works now

--- FrozenFrenzy/Assets/TESTINGSTUFF/SnowballManager.cs
+++ FrozenFrenzy/Assets/TESTINGSTUFF/SnowballManager.cs
@@ -1,17 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SnowballManager : MonoBehaviour
 {
 
-    bool moveUp, moveLeft, moveRight, moveDown;
+    bool moveUp, leftPressed, rightPressed, moveDown;
+    bool canSwap;
+    float movementspeed;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        movementspeed = 0.1f;
     }
 
     // Update is called once per frame
@@ -22,32 +25,52 @@ public class SnowballManager : MonoBehaviour
 
     public void FixedUpdate()
     {
+        gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z + 1.0f * movementspeed);
 
+        if (leftPressed == true)
+        {
+            Debug.Log("LEFT");
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x - 1.0f * movementspeed, gameObject.transform.position.y, gameObject.transform.position.z);
+        }
+        else if (rightPressed == true)
+        {
+            Debug.Log("RIGHT");
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x + 1.0f * movementspeed, gameObject.transform.position.y, gameObject.transform.position.z);
+        }
+        else
+        {
+            Debug.Log("NONE");
+        }
     }
 
+    // ON SCREEN BUTTONS
     public void ResetScene()
     {
         SceneManager.LoadScene(0);
     }
-
-    public void MoveUp()
+    public void MoveLeftPressed()
     {
-        gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z + 1.0f);
+        leftPressed = true;
+    }
+    public void MoveLeftReleased()
+    {
+        leftPressed = false;
+    }
+    public void MoveRightPressed()
+    {
+        rightPressed = true;
+    }
+    public void MoveRightReleased()
+    {
+        rightPressed = false;
     }
 
-    public void MoveLeft()
+    public void LeftExited()
     {
-        gameObject.transform.position = new Vector3(gameObject.transform.position.x - 1.0f, gameObject.transform.position.y, gameObject.transform.position.z);
+        leftPressed = false;
     }
-
-    public void MoveRight()
+    public void RightExited()
     {
-        gameObject.transform.position = new Vector3(gameObject.transform.position.x + 1.0f, gameObject.transform.position.y, gameObject.transform.position.z);
+        rightPressed = false;
     }
-
-    public void MoveDown()
-    {
-        gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z - 1.0f);
-    }
-
 }
