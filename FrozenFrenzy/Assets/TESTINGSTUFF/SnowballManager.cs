c41f2fa661ed24232160dc50437ea7b064ba5686
Maxwell Boucher <112987961+mboucher41@users.noreply.github.com>
2024-02-12 14:40:00 -0700

    Set up basic gameplay functionality

--- FrozenFrenzy/Assets/TESTINGSTUFF/SnowballManager.cs
+++ FrozenFrenzy/Assets/TESTINGSTUFF/SnowballManager.cs
@@ -7,35 +7,63 @@ using UnityEngine.SceneManagement;
 public class SnowballManager : MonoBehaviour
 {
 
-    bool moveUp, leftPressed, rightPressed, moveDown;
-    bool canSwap;
-    float movementspeed;
+    bool leftPressed, rightPressed;
+    public float forwardMovementspeed, sideMovementSpeed;
+
+    public bool playingGame;
+    public bool canAccel;
+    public bool canBoost;
+    public bool canSlow;
 
     // Start is called before the first frame update
     void Start()
     {
-        movementspeed = 0.1f;
+        forwardMovementspeed = 0.0f;
+        sideMovementSpeed = 0.15f;
+        playingGame = false;
+
+        canAccel = true;
+        canBoost = true;
+        canSlow = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (playingGame == true)
+        {
+            if (canAccel == true)
+            {
+                if (forwardMovementspeed <= 0.5f)
+                {
+                    canAccel = false;
+                    StartCoroutine(Accelerate());
+                }
+            }
+        }
     }
 
     public void FixedUpdate()
     {
-        gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z + 1.0f * movementspeed);
+        if (playingGame == true)
+        {
+            MovePlayer();
+        }
+    }
+
+    public void MovePlayer() // Called in FixedUpdate
+    {
+        gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z + 1.0f * forwardMovementspeed);
 
         if (leftPressed == true)
         {
             Debug.Log("LEFT");
-            gameObject.transform.position = new Vector3(gameObject.transform.position.x - 1.0f * movementspeed, gameObject.transform.position.y, gameObject.transform.position.z);
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x - 1.0f * sideMovementSpeed, gameObject.transform.position.y, gameObject.transform.position.z);
         }
         else if (rightPressed == true)
         {
             Debug.Log("RIGHT");
-            gameObject.transform.position = new Vector3(gameObject.transform.position.x + 1.0f * movementspeed, gameObject.transform.position.y, gameObject.transform.position.z);
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x + 1.0f * sideMovementSpeed, gameObject.transform.position.y, gameObject.transform.position.z);
         }
         else
         {
@@ -43,34 +71,43 @@ public class SnowballManager : MonoBehaviour
         }
     }
 
+    public IEnumerator Accelerate() // Called in Update
+    {
+        yield return new WaitForSeconds(0.1f);
+        forwardMovementspeed += 0.01f;
+        canAccel = true;
+    }
+
     // ON SCREEN BUTTONS
-    public void ResetScene()
+    #region
+    public void ResetScene() // ResetButton
     {
         SceneManager.LoadScene(0);
     }
-    public void MoveLeftPressed()
+    public void MoveLeftPressed() // Left Button
     {
         leftPressed = true;
     }
-    public void MoveLeftReleased()
+    public void MoveLeftReleased() // Left Button
     {
         leftPressed = false;
     }
-    public void MoveRightPressed()
+    public void LeftExited() // Left Button
+    {
+        leftPressed = false;
+    }
+
+    public void RightExited() // Right Button
+    {
+        rightPressed = false;
+    }
+    public void MoveRightPressed() // Right Button
     {
         rightPressed = true;
     }
-    public void MoveRightReleased()
+    public void MoveRightReleased() // Right Button
     {
         rightPressed = false;
     }
-
-    public void LeftExited()
-    {
-        leftPressed = false;
-    }
-    public void RightExited()
-    {
-        rightPressed = false;
-    }
+    #endregion
 }
