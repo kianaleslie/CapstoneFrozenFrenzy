057e32f476b3e8d7b825e1c5ab4228a4c1debde1
kiana <kianaleslie25@gmail.com>
2024-02-15 19:53:34 -0700

    added music to test the mute button and completed the snowball select

---
+++ FrozenFrenzy/Assets/Kiana's Work/AudioManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioSource audioSource;
+    bool isMuted = false;
+
+    public GameObject mutedImage;
+
+    private void Start()
+    {
+        mutedImage.SetActive(false);
+    }
+
+    //kiana - this method is used to mute the audio and is used on the mute button on the main menu
+    public void MuteAudio()
+    {
+        isMuted = !isMuted;
+
+        if(isMuted)
+        {
+            audioSource.volume = 0f;
+            mutedImage.SetActive(true);
+        }
+        else
+        {
+            audioSource.volume = 1f;
+            mutedImage?.SetActive(false);
+        }
+    }
+}
