3de527c7df94d810ab811dc4446fd09789b77b2b
kiana <kianaleslie25@gmail.com>
2024-03-05 12:03:47 -0700

    working on snowball sprite and gameobject connection

--- FrozenFrenzy/Assets/Kiana's Work/Scripts/Snowballs.cs
+++ FrozenFrenzy/Assets/Kiana's Work/Scripts/Snowballs.cs
@@ -7,7 +7,6 @@ public class Snowballs : MonoBehaviour
     public SnowBallSelect snowballs;
     public GameObject[] snowballPrefabs;
     
-
     void Start()
     {
         foreach(var snowball in snowballPrefabs)
@@ -15,12 +14,35 @@ public class Snowballs : MonoBehaviour
             snowball.gameObject.SetActive(false);
         }
     }
-
     void Update()
     {
-        //if (snowballs.snowballList[0])
-        //{
-        //    snowballPrefabs
-        //}
+        if (snowballs.snowballList[0])
+        {
+            GameObject snowball1 = snowballPrefabs[0];
+        }
+        if (snowballs.snowballList[1])
+        {
+            GameObject snowball2 = snowballPrefabs[1];
+        }
+        if (snowballs.snowballList[2])
+        {
+            GameObject snowball3 = snowballPrefabs[2];
+        }
+        if (snowballs.snowballList[3])
+        {
+            GameObject snowball4 = snowballPrefabs[3];
+        }
+        if (snowballs.snowballList[4])
+        {
+            GameObject snowball5 = snowballPrefabs[4];
+        }
+        if (snowballs.snowballList[5])
+        {
+            GameObject snowball6 = snowballPrefabs[5];
+        }
+        if (snowballs.snowballList[6])
+        {
+            GameObject snowball7 = snowballPrefabs[6];
+        }
     }
 }
